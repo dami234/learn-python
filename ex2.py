94f10fb077d5266f57334f6c10b7962cae2543d0
Xin Zhou <xzhou688@gmail.com>
2022-02-14 12:58:16 -0500

    Add completed exercise 2

---
+++ ex2.py
@@ -0,0 +1,9 @@
+# A comment, this is so you can read your programm later.
+# Anthing after the # is going ingored by pathon.
+
+print("I could have code like this.")  # and the comment after is ingored.
+
+# You can also use a comment to "disable" or comment out code:
+# print("This won't run.")
+
+print("This will run.")
